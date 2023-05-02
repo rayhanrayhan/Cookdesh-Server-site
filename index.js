a22e51a7295707125c75d48019793968d561a075
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-05-02 23:14:31 +0600

    fix some ui issuse for loading data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors =require('cors');
 const port = process.env.PORT || 5000;
-const cheif = require ('./data/cheif.json');
+const cheif = require('./data/cheif.json');
 
 app.use(cors()) ;
 
